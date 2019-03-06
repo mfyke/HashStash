cfec76384af6f3676c679506204634ca12aed75f
matt fyke <mfyke@live.com>
2019-03-06 13:54:20 -0800

    starting home page

---
+++ client/src/components/AppNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppNav = () => {
+    return (
+        <div>
+            <h1>This is the app nav bar.</h1>
+        </div>
+    );
+}
+
+export default AppNav;
---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+    render() {
+        return (
+            <h1>Yooooo this is the home page ;)</h1>
+        );
+    }
+}
+
+export default Home;
